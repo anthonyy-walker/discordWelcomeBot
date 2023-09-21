702edf41f07d49769022e3cd26008ebaabbf4814
Anthony Walker <145586083+anthonyy-walker@users.noreply.github.com>
2023-09-20 22:28:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client ();
+const Canvas = require('canvas');
+
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+
+
+	let fontSize = 70;
+
+	do {
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	return ctx.font;
+};
+
+bot.on('guildMemberAdd', async member => {
+  console.log("member joined")
+  let guild = bot.guilds.cache.get("")
+	const channel = guild.channels.cache.find(ch => ch.name === 'welcome');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to BFC,', canvas.width / 2.5, canvas.height / 3);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.6);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to BFC, ${member}!`, attachment);
+});
+
+bot.login("");
